2b105196079b3877ce31b50bafa6e1c7473f0420
Richard Frank <rich@quantopian.com>
2016-03-01 17:03:34 -0500

    Get ta-lib

--- conda/ta-lib/bld.bat
+++ conda/ta-lib/bld.bat
@@ -1,2 +1,7 @@
-%SYS_PYTHON% setup.py build --compiler msvc
-%SYS_PYTHON% setup.py install --prefix=%PREFIX%
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-msvc.zip', '%PREFIX%/ta-lib-0.4.0-msvc.zip')"
+IF %ERRORLEVEL% == 1; exit 1
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem;[System.IO.Compression.ZipFile]::ExtractToDirectory('%PREFIX%/ta-lib-0.4.0-msvc.zip', 'C:\')"
+IF %ERRORLEVEL% == 1; exit 1
+
+python setup.py build --compiler msvc
+python setup.py install --prefix=%PREFIX%
