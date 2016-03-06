2782f6c03727bd5af85870970ea19500e9ead613
Richard Frank <rich@quantopian.com>
2016-03-05 22:05:08 -0500

    more

--- conda/ta-lib/bld.bat
+++ conda/ta-lib/bld.bat
@@ -3,18 +3,30 @@ IF %ERRORLEVEL% == 1; exit 1
 powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem;[System.IO.Compression.ZipFile]::ExtractToDirectory('ta-lib-0.4.0-msvc.zip', 'C:\')"
 IF %ERRORLEVEL% == 1; exit 1
 pushd C:\ta-lib\c\
-cd make\cdd\win32\msvc
+pushd make\cdd\win32\msvc
 nmake
-cd ..\cdr\win32\msvc
+IF %ERRORLEVEL% == 1; exit 1
+popd
+pushd make\cdr\win32\msvc
 nmake
-cd ..\cmd\win32\msvc
+IF %ERRORLEVEL% == 1; exit 1
+popd
+pushd make\cmd\win32\msvc
 nmake
-cd ..\cmr\win32\msvc
+IF %ERRORLEVEL% == 1; exit 1
+popd
+pushd make\cmr\win32\msvc
 nmake
-cd ..\csd\win32\msvc
+IF %ERRORLEVEL% == 1; exit 1
+popd
+pushd make\csd\win32\msvc
 nmake
-cd ..\csr\win32\msvc
+IF %ERRORLEVEL% == 1; exit 1
+popd
+pushd make\csr\win32\msvc
 nmake
+IF %ERRORLEVEL% == 1; exit 1
+popd
 popd
 del ta-lib-0.4.0-msvc.zip
 
